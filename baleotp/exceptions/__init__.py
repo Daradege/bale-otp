b915888703e2b0bf6ebb2326c903da92b7336e04
Aydin <154819637+Code-Wizaard@users.noreply.github.com>
2025-07-05 17:41:55 +0330

    Feat:Added exception classes

---
+++ baleotp/exceptions/__init__.py
@@ -0,0 +1,18 @@
+class PbbOtpException(Exception):
+    pass
+
+class UnauthorizedException(PbbOtpException):
+    pass
+
+class BadRequest(PbbOtpException):
+    pass
+
+class InternalServerError(PbbOtpException):
+    pass
+
+class UserNotFound(PbbOtpException):
+    pass
+
+class PaymentRequired(PbbOtpException):
+    pass
+
